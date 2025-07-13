74a3fc06896c2a75aa8f4508c33ea142a6cf2f1f
Mazune1995 <1995nasser@gmail.com>
2025-07-13 11:11:38 -0700

    the file

--- Introduction_to_Django/LibraryProject/LibraryProject/wsgi.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/wsgi.py
@@ -1,9 +1,8 @@
-cat > LibraryProject/wsgi.py <<EOF
 import os
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 
 application = get_wsgi_application()
-EOF
 
