67552fb0cf3e0270193aeffdbba9bc01aea11a4b
Mazune1995 <1995nasser@gmail.com>
2025-07-20 13:10:18 -0700

    The File

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,5 +20,14 @@ urlpatterns = [
     # Django built-in auth views with custom templates
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
 ]
+
+    ]
 
