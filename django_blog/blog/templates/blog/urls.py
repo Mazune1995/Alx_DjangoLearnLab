a83210789478db0913fe45ff23284f874c12869e
Mazune1995 <1995nasser@gmail.com>
2025-08-16 06:43:44 -0700

    my file

--- django_blog/blog/templates/blog/urls.py
+++ django_blog/blog/templates/blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from . import views
+from .views import (
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+)
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile_view, name='profile'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
 
