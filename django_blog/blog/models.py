efe4a318daa6d3db50ddbd8278b36eee577aaccc
Mazune1995 <1995nasser@gmail.com>
2025-08-16 07:04:38 -0700

    The file

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,6 @@
+# Example Post model
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -11,7 +11,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
