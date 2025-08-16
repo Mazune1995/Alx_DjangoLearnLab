b5c3d783325c73f813562b7c3aaeff9b92712f42
Mazune1995 <1995nasser@gmail.com>
2025-08-16 07:09:13 -0700

    the file

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,76 +1,43 @@
-# For function-based views
-from django.contrib.auth.decorators import login_required
-
-# For class-based views
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from .models import Post
+from django.views.generic import CreateView, UpdateView, DeleteView
+from .models import Comment
 
-# List all posts
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'  # blog/templates/blog/post_list.html
-    context_object_name = 'posts'
-    ordering = ['-created_at']
-
-# View a single post
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
-
-# Create a new post
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        form.instance.post_id = self.kwargs['post_id']  # link comment to post
         return super().form_valid(form)
 
-# Update a post
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
 
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-    from django.views.generic import UpdateView
-from .models import Post
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    fields = ['title', 'content']
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'blog/comment_form.html'
 
     def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
 
 
-# Delete a post
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    success_url = '/'
-    
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_confirm_delete.html'
+
     def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
+        comment = self.get_object()
+        return self.request.user == comment.author
 
-@login_required
-def some_view(request):
-    # Only logged-in users can access
-    ...
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
 
