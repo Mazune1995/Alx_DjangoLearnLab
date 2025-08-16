97f4883d5f5e00f75c9ff85229e19a7b80b9f31b
Mazune1995 <1995nasser@gmail.com>
2025-08-16 07:15:05 -0700

    The file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from .views import (
-    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-)
+from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    # ... existing post URLs ...
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
-
