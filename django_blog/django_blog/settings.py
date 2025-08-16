7cbd75ac37107722a3281bd42614d94c78d2e87e
Mazune1995 <1995nasser@gmail.com>
2025-08-16 05:39:10 -0700

    the file

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # or mysql
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',  # or your DB host
+        'PORT': '5432',       # or 3306 for MySQL
     }
 }
 # Password validation
@@ -143,4 +147,3 @@ TEMPLATES = [
         },
     },
 ]
-
